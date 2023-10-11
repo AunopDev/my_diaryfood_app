b4fa028e6638d914be918f28aaf0e4adc0182cef
AunopDev <s6452410004@sau.ac.th>
2023-10-11 16:45:51 +0700

    Code V2 : AunopDev

---
+++ lib/views/add_diaryfood.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddDairyfoodUI extends StatefulWidget {
+  const AddDairyfoodUI({super.key});
+
+  @override
+  State<AddDairyfoodUI> createState() => _AddDairyfoodUIState();
+}
+
+class _AddDairyfoodUIState extends State<AddDairyfoodUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
